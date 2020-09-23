37196695954b7fc8a06d9aca82e291dd8dbddab6
Psmths <psmth@protonmail.ch>
2020-09-23 12:10:46 -0500

    add arg escape

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
   if ($_SERVER[REQUEST_URI] == "/$base/create" or $_SERVER[REQUEST_URI] == "/$base/create/"){
     require("shortener.html");
     if ($_POST["url"]){
-      $shortener = exec("python3 main.py --create ".escapeshellcmd($_POST["url"])." 2>&1");
+      $shortener = exec(escapeshellcmd("python3 main.py --create ".escapeshellarg($_POST["url"])." 2>&1"));
       echo($shortener);
     }
   }else{
-    $dest = exec("python3 main.py --request ".escapeshellcmd($request)." 2>&1");
+    $dest = exec(escapeshellcmd("python3 main.py --request ".escapeshellarg($request)." 2>&1"));
     if ($dest == "None"){
       http_response_code(404);
       echo("404: Resource not found.");
